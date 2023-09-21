892836d2f5cac61c6f577cb9089b78f05b57919c
giacob500 <giacob500@gmail.com>
2023-09-21 16:50:40 +0100

    Workbook challenge 2

--- first.py
+++ first.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import random
+import string
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Napier'
+    result = "<h1>The following is a random character:</h1>" + ''.join(random.choices(string.ascii_letters))
+    return result
