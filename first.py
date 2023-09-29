2fed0e9fa3330ce290f2aea5472c5411419ffbd0
giacob500 <giacob500@gmail.com>
2023-09-29 12:44:27 +0100

    template collection

--- first.py
+++ first.py
@@ -58,6 +58,11 @@ def account():
 def hello(name=None):
     return render_template('conditional.html', name=name)
 
+@app.route('/users/')
+def users():
+    names=['simon','thomas','lee','jamie','sylvester']
+    return render_template('loops.html', names=names)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
