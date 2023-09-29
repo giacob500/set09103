766fa126402dd1d07032db767af14dc1bb771f87
giacob500 <giacob500@gmail.com>
2023-09-29 13:08:48 +0100

    Template Inheritance

--- first.py
+++ first.py
@@ -63,6 +63,19 @@ def users():
     names=['simon','thomas','lee','jamie','sylvester']
     return render_template('loops.html', names=names)
 
+@app.route('/inherits/')
+def inherits():
+    return render_template('base.html')
+
+@app.route('/inherits/one/')
+def inherits_one():
+    return render_template('inherits1.html')
+
+@app.route('/inherits/two/')
+def inherits_two():
+    return render_template('inherits2.html')
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
