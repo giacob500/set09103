454b08ff18125aa2792d6e4764444ee71b82af6a
GIacomo Lorenzi <giacob500@gmail.com>
2023-10-01 14:28:58 +0100

    config file

--- first.py
+++ first.py
@@ -1,81 +1,39 @@
-from flask import Flask, redirect, url_for, abort, request, render_template
-import random
-import string
+import configparser
+
+from flask import Flask
 
 app = Flask(__name__)
 
+def init(app):
+    config = configparser.ConfigParser()
+    try:
+        print("INIT FUNCTION")
+        config_location = "etc/defaults.cfg"
+        config.read(config_location)
+
+        app.config['DEBUG'] = config.get("config", "debug")
+        app.config['ip_address'] = config.get("config", "ip_address")
+        app.config['port'] = config.get("config", "port")
+        app.config['url'] = config.get("config", "url")
+    except:
+        print ("Could not read configs from: ", config_location)
+
+init(app)
+
 @app.route('/')
-def hello_world():
-    result = "<h1>The following is a random character:</h1>" + ''.join(random.choices(string.ascii_letters))
-    return result
+def root():
+    return "Hello Napier from the configuration testing app"
 
-@app.route("/private")
-def private():
-    #Test for user logged in failed so redirect to login URL
-    return redirect (url_for('login'))
-
-@app.route('/login')
-def login():
-    return "Now we would get username & password"
-
-@app.route('/force404')
-def force404():
-    abort(404)
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return "Couldn't find the page you requested.", 404
-
-@app.route('/static-example/img')
-def static_example_img():
-    start = '<img src="'
-    url = url_for('static', filename='vmask.jpg')
-    end = '">'
-    return start+url+end, 200
-
-@app.route("/display/")
-def display():
-    return '<img src="'+url_for('static', filename='uploads/file.png')+'"/>'
-
-@app.route("/upload/", methods=['POST','GET'])
-def account():
-    if request.method == 'POST':
-        f = request.files['datafile']
-        f.save('static/uploads/file.png')
-        return "File Uploaded"
-    else:
-        page ='''
-        <html><body>
-        <form action="" method="post" name="form" enctype="multipart/form-data">
-        <input type="file" name="datafile" />
-        <input type="submit" name="submit" id="submit"/>
-        </form>
-        </body><html>'''
-        return page, 200
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('conditional.html', name=name)
-
-@app.route('/users/')
-def users():
-    names=['simon','thomas','lee','jamie','sylvester']
-    return render_template('loops.html', names=names)
-
-@app.route('/inherits/')
-def inherits():
-    return render_template('base.html')
-
-@app.route('/inherits/one/')
-def inherits_one():
-    return render_template('inherits1.html')
-
-@app.route('/inherits/two/')
-def inherits_two():
-    return render_template('inherits2.html')
-
-
+@app.route('/config/')
+def config():
+    s = []
+    s.append('debug:'+str(app.config['DEBUG']))
+    s.append('port:'+app.config['port'])
+    s.append('ip_address:'+app.config['ip_address'])
+    return ', '.join(s)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    init(app)
+    app.run(
+        host=app.config['ip_address'],
+        port=int(app.config['port']))
