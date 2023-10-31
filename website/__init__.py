b10b8d1f0e07bfda53946455b3ac7c210fc99f36
GIacomo Lorenzi <giacob500@gmail.com>
2023-10-31 10:18:19 +0000

    v 0.1.1 - Added Login page
    Login page redirects user to homepage after clicking on "login". No check of credentials is performed, so anyone can login as long as they insert a valid email in the corresponsive field.
    Also included jinja2 in all website pages to retrieve common headers, footer and JavaScript.

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 # Testing
@@ -11,25 +11,37 @@ def test():
 def home():
     return render_template("index.html")
 
-@app.route("/shop.html")
+@app.route("/shop")
 def shop():
     return render_template("shop.html")
 
-@app.route("/collections.html")
+@app.route("/collections")
 def collections():
     return render_template("collections.html")
 
-@app.route("/categories.html")
+@app.route("/categories")
 def categories():
     return render_template("categories.html")
 
-@app.route("/product.html")
+@app.route("/product")
 def product():
     return render_template("product.html")
 
-@app.route("/basket.html")
+@app.route("/basket")
 def basket():
     return render_template("basket.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["email"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return redirect(url_for('home'))
 
 # Redirect addresses
 @app.route('/index.html')
