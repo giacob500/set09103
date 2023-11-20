fee5ff71d3b96463c5cf831e9aee7156b2b5e75c
GIacomo Lorenzi <giacob500@gmail.com>
2023-11-20 20:04:23 +0000

    v 0.1.10.5 - Improved email generation
    Release notes:
    -Now the user can fill the fields in the basket page to generate the draft of an order email with relative content.

---
+++ website/static/scripts/email_script.js
@@ -0,0 +1,37 @@
+document.getElementById('emailForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var formElements = document.getElementById('emailForm').elements;
+    var bodyContent = '—————————————————\n';
+    
+    for (var i = 0; i < formElements.length; i++) {
+        var element = formElements[i];
+        
+        // Check if the element is a hidden input
+        if (element.type === 'hidden') {
+            bodyContent += element.name + ': ' + element.value + '\n';
+            if (element.name === 'Quantity') {
+                bodyContent += '\n';
+            }
+        }
+    }
+
+    bodyContent += '—————————————————';
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    if (document.getElementById('comments').value == ''){
+        var comments = "none"
+    } else {
+        var comments = document.getElementById('comments').value;
+    }
+
+    var mailtoLink = 'mailto:lorenzi@lorenzi.net?subject=New%20Order%20from%20SVE%20website%20-%20'
+    + encodeURIComponent(name) + '&body=Hi%2C%0D%0Aplease%20check%20out%20the%20order%20below%3A%0D%0A%0D%0A' + encodeURIComponent(bodyContent) + '%0D%0AAdditional%20comments%3A%0D%0A'
+    + encodeURIComponent(comments) + '%0D%0A%0D%0AMany%20thanks%2C%0D%0A' + encodeURIComponent(name) + '%0D%0A%0D%0A' + encodeURIComponent(email)
+    + '%0D%0A' + encodeURIComponent(phone);;
+
+    // Open default email client
+    window.location.href = mailtoLink;
+});
