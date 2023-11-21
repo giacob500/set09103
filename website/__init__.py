eabbce5caa93aa851d55d2056b5289948f5d5640
GIacomo Lorenzi <giacob500@gmail.com>
2023-11-21 18:13:43 +0000

    v 0.1.11 - Password encryption
    Release notes:
    -When registering a new user, passwords are now encrypted using Bcrypt
    - Enhanced display of flash messages in login and register page
    -Fixed login bugs
    -Improved categories page UI

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask, flash, redirect, url_for, render_template, request, session
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.secret_key = "notasecret"
+app.config['SECRET_KEY'] = 'not_a_secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../var/users.sqlite3'
 app.config["SQLALCHEMY_TRACK_NOTIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=15)
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 class users(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
@@ -133,18 +135,28 @@ def basket():
             return render_template("basket.html", basket_data=session["basket_data"])
         return render_template("basket.html")
     flash("Please log-in to access this page", "info")
-    return redirect(url_for("login"))
+    return redirect(url_for("user"))
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
-    if request.method == "POST":
-        email = request.form['email']
-        password = request.form['pswd']
-        new_user = users(email=email, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-        flash("Registration successful. Please log in.", "success")
-        return redirect(url_for("login"))
+    if "email" in session:
+        if request.method == "POST":
+            email = request.form['email']
+            password = request.form['pswd']
+
+            # Check if the email is already in use
+            existing_user = users.query.filter_by(email=email).first()
+            if existing_user:
+                flash('Email already in use. Please choose another email.', 'error')
+                return redirect(url_for('register'))
+
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            new_user = users(email=email, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Registration successful. Please log in.", "success")
+            return redirect(url_for("login"))
+        return redirect(url_for("user"))
     return render_template("register.html")
 
 # Check if user is loggin in for the first time or is already logged in
@@ -154,30 +166,28 @@ def login():
         session.permanent = True 
         email = request.form["email"]
         password = request.form["pswd"]
+
+        # Check if there is any corresponding email in db
         found_user = users.query.filter_by(email=email).first()
-        # Check if there is any corresponding user in db
-        if found_user:
-            if found_user.email == email and found_user.password == password:
-                session["email"] = found_user.email
-                # Login successful
-                return redirect(url_for('user'))
+        if found_user and bcrypt.check_password_hash(found_user.password, password):
+            # Login successful
+            session["email"] = found_user.email
+            return redirect(url_for('user'))
         else:
             flash('Login Failed, please check email and password.', 'error')
         return render_template("login.html")
-    else:
-        # If there is already an email in the session and it corresponds to an email in the db
-        #if "email" in session and users.query.filter_by(email=session["email"]).first():
-        #    flash("You are already logged in", "info")
-        #    return redirect(url_for("user"))
-        return render_template("login.html")
+    # If there is already an email in the session check who they are
+    if "email" in session:
+        return redirect(url_for("user"))
+    return render_template("login.html")
     
 # Check if user is already logged in, otherwise prompt login screen
 @app.route("/user")
 def user():
     # If there is already an email in the session and it corresponds to an email in the db
-    if "email" in session and users.query.filter_by(email=session["email"]).first():
+    if users.query.filter_by(email=session["email"]).first():
+        flash("You are already logged in", "info")
         return redirect(url_for("home"))
-        #return render_template("categories.html", username=registered_users.get(usr, "User"))
     else:
         return redirect(url_for("login"))
 
